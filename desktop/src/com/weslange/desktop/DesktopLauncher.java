76292eebe8c65c4bbef70c2d5124e5042c749827
Wes Lange <4862186+shatterblast@users.noreply.github.com>
2019-06-06 20:23:49 -0500

    Add files via upload

---
+++ desktop/src/com/weslange/desktop/DesktopLauncher.java
@@ -0,0 +1,41 @@
+package com.weslange.desktop;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+
+import com.weslange.GdxGameSvcsApp;
+import com.weslange.GpgsMappers;
+import de.golfgl.gdxgamesvcs.GpgsClient;
+import de.golfgl.gdxgamesvcs.IGameServiceIdMapper;
+
+public class DesktopLauncher {
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new GdxGameSvcsApp() {
+            @Override
+            public void create() {
+                gsClient = new GpgsClient() {
+                    @Override
+                    public boolean submitEvent(String eventId, int increment) {
+                        return super.submitEvent(GpgsMappers.mapToGpgsEvent(eventId), increment);
+                    }
+                }.setGpgsAchievementIdMapper(new IGameServiceIdMapper<String>() {
+                    @Override
+                    public String mapToGsId(String independantId) {
+                        return GpgsMappers.mapToGpgsAchievement(independantId);
+                    }
+                }).setGpgsLeaderboardIdMapper(new IGameServiceIdMapper<String>() {
+                    @Override
+                    public String mapToGsId(String independantId) {
+                        return GpgsMappers.mapToGpgsLeaderboard(independantId);
+                    }
+                }).initialize("gdx-gamesvcs-desktop-gpgs",
+                        Gdx.files.internal("gpgs-client_secret.json"), true);
+                super.create();
+
+            }
+        }, config);
+    }
+}
